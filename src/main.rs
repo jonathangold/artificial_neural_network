b6b1d2d4c157e06e73ff280ee55fc2ef112bd4ca
Jonathan Gold <jonathan.e.gold@gmail.com>
2017-08-25 22:52:50 -0400

    needs debugging, training not working

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,12 @@ mod perceptron;
 mod train;
 
 fn main() {
-    }
+    let data: Vec<(i32,i32)> = vec!((0,0),(0,1),(1,0),(1,1));
+    let xorResult: Vec<i32> = vec!(0,1,1,0);
+    
+    let mut xorPerceptron = perceptron::newPerceptron(2);
+
+    println!("{:?}", &xorPerceptron);
+    train::train(&mut xorPerceptron, 10000, 0.1);
+    println!("{:?}", &xorPerceptron);
+}
--- src/train.rs
+++ src/train.rs
@@ -20,19 +20,25 @@ fn isAboveLine(point:&Vec<i32>, f:&Fn(i32) -> i32) -> i32 {
         _ => {0}
     }
 }
-
-fn train(p: perceptron::Perceptron, iters: i32, rate: f32) {
+pub fn train(p: &mut perceptron::Perceptron, iters: i32, rate: f32) {
+    let mut counter = 0;
     for i in 1..iters {
         let mut point: Vec<i32> = Vec::new();
         point.push(randInt());
         point.push(randInt());
-        let x = point[0];
         let actual = p.process(&point);
         let expected = isAboveLine(&point, &f);
         let delta = expected - actual;                           
+        p.adjust(point, delta, rate);
+        if delta == 0 {
+            counter += 1;
+        } else {
+            continue;
+        }
     }
+        println!("{}", counter);
 }
 
 fn randInt() -> i32 {
-    rand::thread_rng().gen_range(-100, 101)   
+    rand::thread_rng().gen_range(-100, 201)   
 }
